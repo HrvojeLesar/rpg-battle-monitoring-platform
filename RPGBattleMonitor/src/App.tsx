689ed83ba47b09ac83cd448bf6dd22d835efdbab
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2024-06-25 14:56:52 +0200

    feat: very basic object moving and canvas setup

--- RPGBattleMonitor/src/App.tsx
+++ RPGBattleMonitor/src/App.tsx
@@ -1,7 +1,7 @@
 import { Sprite } from "pixi.js";
 import "./App.css";
-import { useViewport } from "./hooks/useCanvasContext";
-import { useEffect, useState } from "react";
+import { useApp, useViewport } from "./hooks/useCanvasContext";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { Slider } from "antd";
 
 const style: React.CSSProperties = {
@@ -16,9 +16,28 @@ function App() {
 
     const [height, setHeight] = useState(box.height);
     const [width, setWidth] = useState(box.width);
+    const [selection, setSelection] = useState<string | null>(null);
+
+    useMemo(() => {
+        document.addEventListener("testevent", (e) => {
+            setSelection(e.detail);
+        });
+    }, []);
+
     useEffect(() => {
+        const interval = setInterval(() => {
+            if (width > 100) {
+                setWidth(10);
+            } else {
+                setWidth(width + 1);
+            }
+        }, 10);
         box.height = height;
         box.width = width;
+
+        return () => {
+            clearTimeout(interval);
+        };
     }, [height, width]);
 
     return (
@@ -34,6 +53,7 @@ function App() {
                     onChange={setHeight}
                 />
             </div>
+            <div> {`${selection}`} </div>
         </>
     );
 }
--- RPGBattleMonitor/src/initUtil.ts
+++ RPGBattleMonitor/src/initUtil.ts
@@ -1,8 +1,14 @@
 import { Viewport } from "pixi-viewport";
-import { Application, Sprite, Texture } from "pixi.js";
+import {
+    Application,
+    FederatedPointerEvent,
+    Point,
+    Sprite,
+    Texture,
+} from "pixi.js";
 import { CanvasContextType } from "./context/CanvasContext";
 
-export const createCanvasAppAndViewport = async (): Promise<CanvasContextType> => {
+export async function createCanvasAppAndViewport(): Promise<CanvasContextType> {
     const app = new Application();
     await app.init();
 
@@ -18,10 +24,72 @@ export const createCanvasAppAndViewport = async (): Promise<CanvasContextType> =
     viewport.drag().pinch().wheel().decelerate();
 
     // add a red box
-    const sprite = viewport.addChild(new Sprite(Texture.WHITE));
+    let sprite = viewport.addChild(new Sprite(Texture.WHITE));
     sprite.tint = 0xff0000;
     sprite.width = sprite.height = 100;
     sprite.position.set(100, 100);
+    sprite.eventMode = "static";
+
+    sprite.on("pointerdown", (event) => {
+        console.log("sprite 1");
+        document.dispatchEvent(
+            new CustomEvent("testevent", { detail: event.target.label }),
+        );
+    });
+
+    sprite = viewport.addChild(new Sprite(Texture.WHITE));
+    sprite.tint = 0xff00ff;
+    sprite.width = sprite.height = 100;
+    sprite.position.set(200, 100);
+    sprite.eventMode = "static";
+    sprite.cursor = "grabbing";
+    sprite.label = "test sprite";
+
+    let dragTarget: Sprite | null = null;
+    let position: Point = new Point();
+
+    function onDragEnd(_event: FederatedPointerEvent) {
+        if (dragTarget) {
+            viewport.off("pointermove", onDragMove);
+            dragTarget.alpha = 1;
+            dragTarget = null;
+            viewport.pause = false;
+        }
+    }
+
+    viewport.on("pointerup", onDragEnd);
+    viewport.on("pointerupoutside", onDragEnd);
+
+    function onDragMove(event: FederatedPointerEvent) {
+        if (dragTarget) {
+            const localPos = event.getLocalPosition(dragTarget.parent);
+            dragTarget.position.set(
+                localPos.x - position.x,
+                localPos.y - position.y,
+            );
+        }
+    }
+
+    sprite.on(
+        "pointerdown",
+        (event) => {
+            viewport.pause = true;
+            sprite.alpha = 0.5;
+            dragTarget = sprite;
+            const localPos = event.getLocalPosition(sprite.parent);
+            position.x = localPos.x - sprite.position.x;
+            position.y = localPos.y - sprite.position.y;
+            viewport.on("pointermove", onDragMove);
+        },
+        sprite,
+    );
+
+    sprite.on("pointerdown", (event) => {
+        console.log("sprite 2");
+        document.dispatchEvent(
+            new CustomEvent("testevent", { detail: event.target.label }),
+        );
+    });
 
     return { app, viewport };
-};
+}
