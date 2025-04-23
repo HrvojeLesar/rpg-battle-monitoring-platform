02e3dd593bbcf520b3dadb3a5ddd88cab728693b
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2025-04-23 12:40:33 +0200

    Apidoc conditional compilation

--- RPGBattleMonitor/rpg_battle_monitor_lib/src/webserver/routes/apidoc/mod.rs
+++ RPGBattleMonitor/rpg_battle_monitor_lib/src/webserver/routes/apidoc/mod.rs
@@ -2,17 +2,19 @@ use axum::Router;
 use utoipa::OpenApi;
 use utoipa_scalar::{Scalar, Servable};
 
+#[cfg(feature = "api_v1_doc")]
 #[derive(OpenApi)]
 #[openapi(info(description = "My Api description"), paths(get_api_doc_router))]
-#[cfg(feature = "api_v1_doc")]
 pub struct ApiDoc;
 
 #[cfg(feature = "api_v1_doc")]
-#[utoipa::path(
-    get,
-    path = "/api/v1/docs", 
-    responses(
-        (status = 200, description = "Api Documentation Page")
+#[cfg_attr(feature = "api_v1_doc",
+    utoipa::path(
+        get,
+        path = "/api/v1/docs", 
+        responses(
+            (status = 200, description = "Api Documentation Page")
+        )
     )
 )]
 pub(super) fn get_api_doc_router<S>() -> Router<S>
