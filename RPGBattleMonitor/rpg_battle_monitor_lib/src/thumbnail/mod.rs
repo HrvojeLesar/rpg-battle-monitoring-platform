548e3051d9c21ae4c2d0772168bf63a168d05b89
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2025-06-12 11:15:57 +0200

    reworked thumbnail generator to be lazy and only create thumbnails when requesting a thumbnail

--- RPGBattleMonitor/rpg_battle_monitor_lib/src/thumbnail/mod.rs
+++ RPGBattleMonitor/rpg_battle_monitor_lib/src/thumbnail/mod.rs
@@ -1,41 +1,86 @@
-use std::io::{BufRead, Seek, Write};
+use std::{
+    cell::OnceCell,
+    io::{BufRead, Seek, Write},
+    sync::Arc,
+};
 
 use crate::thumbnail::{self, configuration::Configuration};
-use image::{DynamicImage, ImageFormat, ImageReader};
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use image::{DynamicImage, GenericImageView, ImageFormat, ImageReader};
 
 pub mod configuration;
 pub mod error;
 pub mod size;
 
-pub struct Thumbnail {
-    thumbnail: DynamicImage,
+pub struct Thumbnail<'a> {
+    original_image: Arc<DynamicImage>,
+    thumbnail: OnceCell<DynamicImage>,
     format: ImageFormat,
+    configuration: &'a Configuration<'a>,
 }
 
-impl Thumbnail {
-    pub fn new(image: &DynamicImage, format: ImageFormat, conf: &Configuration) -> Self {
-        let (width, height) = conf.size.get_dimensions();
-        let thumbnail = image.thumbnail(width, height);
-
-        Self { thumbnail, format }
+impl<'a> Thumbnail<'a> {
+    pub fn new(
+        image: Arc<DynamicImage>,
+        format: ImageFormat,
+        configuration: &'a Configuration<'a>,
+    ) -> Self {
+        Self {
+            thumbnail: OnceCell::new(),
+            format,
+            original_image: image,
+            configuration,
+        }
     }
 
     pub fn write_to<W: Write + Seek>(self, writer: &mut W) -> Result<(), thumbnail::error::Error> {
-        self.thumbnail.write_to(writer, self.format)?;
-
+        let thumbnail = self.get_thumbnail_image();
+        thumbnail.write_to(writer, self.format)?;
         Ok(())
+    }
+
+    /// Gets the thumbnail
+    /// If it does not exist this funcion will generate one
+    /// and will not make the size of the thumbnail any largegr
+    /// than the original image
+    pub fn get_thumbnail_image(&self) -> &DynamicImage {
+        if self.thumbnail.get().is_none() {
+            let thumbnail = self.generate_thumbnail(self.get_dimensions());
+
+            self.thumbnail
+                .set(thumbnail)
+                .expect("Tried to set cell twice");
+        }
+
+        self.thumbnail
+            .get()
+            .expect("Thumbnail should always be present")
+    }
+
+    fn generate_thumbnail(&self, dimensions: (u32, u32)) -> DynamicImage {
+        let (width, height) = dimensions;
+
+        self.original_image.thumbnail(width, height)
+    }
+
+    fn get_dimensions(&self) -> (u32, u32) {
+        let (width, height) = self.configuration.size.get_dimensions();
+        let (o_width, o_height) = self.original_image.dimensions();
+        if width * height > o_width * o_height {
+            (o_width, o_height)
+        } else {
+            (width, height)
+        }
     }
 }
 
-pub fn create_thumbnails<R: BufRead + Seek>(
+pub fn create_thumbnails<'a, R: BufRead + Seek>(
     reader: R,
-    configurations: &[thumbnail::Configuration],
+    configurations: &'a [thumbnail::Configuration<'a>],
     default_format: Option<ImageFormat>,
-) -> Result<Vec<Thumbnail>, thumbnail::error::Error> {
+) -> Result<Vec<Thumbnail<'a>>, thumbnail::error::Error> {
     let image_reader = ImageReader::new(reader).with_guessed_format()?;
     let reader_image_format = image_reader.format();
-    let image = image_reader.decode()?;
+    let image = Arc::new(image_reader.decode()?);
 
     let format = match reader_image_format {
         Some(f) => f,
@@ -43,8 +88,8 @@ pub fn create_thumbnails<R: BufRead + Seek>(
     };
 
     let thumbnails = configurations
-        .into_par_iter()
-        .map(|conf| Thumbnail::new(&image, format, conf))
+        .iter()
+        .map(|conf| Thumbnail::new(image.clone(), format, conf))
         .collect();
 
     Ok(thumbnails)
