a474e29a83c58ecd06239dcd4445b6d4c69b61ee
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2025-06-15 15:03:26 +0200

    transaction request extractor

--- RPGBattleMonitor/rpg_battle_monitor_lib/src/database/transaction.rs
+++ RPGBattleMonitor/rpg_battle_monitor_lib/src/database/transaction.rs
@@ -1,6 +1,9 @@
-use std::fmt::Debug;
+use std::{fmt::Debug, marker::PhantomData};
 
+use axum::{extract::FromRequestParts, http::request::Parts, response::IntoResponse};
 use sqlx::Pool;
+
+use crate::webserver::router::app_state::AppStateTrait;
 
 use super::error::{Error, Result};
 
@@ -13,18 +16,20 @@ impl<DB: sqlx::Database> DatabaseMarker for Pool<DB> {
 }
 
 #[derive(Debug)]
-pub struct Transaction<DB: DatabaseMarker> {
+pub struct Transaction<DB: DatabaseMarker, E = Error> {
     pub db: sqlx::Pool<DB::Driver>,
     transaction: Option<sqlx::Transaction<'static, DB::Driver>>,
     counter: u32,
+    _error: PhantomData<E>,
 }
 
-impl<DB: DatabaseMarker> Transaction<DB> {
+impl<DB: DatabaseMarker, E> Transaction<DB, E> {
     pub fn new(db: Pool<DB::Driver>) -> Self {
         Self {
             db,
             transaction: Option::default(),
             counter: 0,
+            _error: PhantomData,
         }
     }
 
@@ -65,5 +70,26 @@ impl<DB: DatabaseMarker> Transaction<DB> {
         } else {
             Err(Error::NoTransaction)
         }
+    }
+}
+
+impl<DB: DatabaseMarker + sqlx::Database, S, E> FromRequestParts<S> for Transaction<DB, E>
+where
+    S: AppStateTrait<DB::Driver> + Sync,
+    E: From<Error> + IntoResponse + Send,
+{
+    type Rejection = E;
+
+    async fn from_request_parts(
+        parts: &mut Parts,
+        state: &S,
+    ) -> core::result::Result<Self, Self::Rejection> {
+        let pool: Result<&Pool<DB::Driver>> = parts.extensions.get().ok_or(Error::NoExtension);
+        let pool = match pool {
+            Ok(p) => p.clone(),
+            Err(_) => state.get_db_pool(),
+        };
+
+        Ok(Transaction::new(pool))
     }
 }
