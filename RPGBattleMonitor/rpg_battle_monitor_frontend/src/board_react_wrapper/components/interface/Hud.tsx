3f853316a8cfbffdd4d0e58056474d42b28adf2c
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2025-09-16 15:49:42 +0200

    Hud side flexbox

--- RPGBattleMonitor/rpg_battle_monitor_frontend/src/board_react_wrapper/components/interface/Hud.tsx
+++ RPGBattleMonitor/rpg_battle_monitor_frontend/src/board_react_wrapper/components/interface/Hud.tsx
@@ -1,11 +1,20 @@
-import { Box, Button } from "@mantine/core";
-import classes from "../../../css/hud.module.css";
+import { Box, Button, Flex } from "@mantine/core";
 import { TokenFactory } from "../../../board_core/factories/token_factory";
 import { useAtomValue } from "jotai";
 import { sceneAtoms } from "../../stores/board_store";
 import { SceneSelection } from "./SceneSelection";
+import classes from "../../../css/hud.module.css";
+import { ReactNode } from "react";
 
-export const Hud = () => {
+const SidesFlexBox = ({ children }: { children?: ReactNode }) => {
+    return (
+        <Flex direction="column" m="md" gap="xs">
+            {children}
+        </Flex>
+    );
+};
+
+const HudLeft = () => {
     const currentScene = useAtomValue(sceneAtoms.getCurrentScene);
 
     const addTokenButton = () => {
@@ -23,10 +32,29 @@ export const Hud = () => {
             </Button>
         );
     };
+
     return (
-        <Box className={classes.hud}>
+        <SidesFlexBox>
             <SceneSelection />
             {addTokenButton()}
+        </SidesFlexBox>
+    );
+};
+
+const HudFiller = () => {
+    return <div></div>;
+};
+
+const HudRight = () => {
+    return <SidesFlexBox></SidesFlexBox>;
+};
+
+export const Hud = () => {
+    return (
+        <Box className={classes.hud}>
+            <HudLeft />
+            <HudFiller />
+            <HudRight />
         </Box>
     );
 };
